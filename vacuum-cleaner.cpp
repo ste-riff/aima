f2f700db63519c489b1e8dba9b6ccd644b43ee44
Stefano Savarino <ste.riff@gmail.com>
2017-02-23 22:06:59 +0100

    Code moved to appropriate functions.

--- vacuum-cleaner.cpp
+++ vacuum-cleaner.cpp
@@ -5,50 +5,83 @@
 
 const unsigned int lifetime = 1000;
 enum action {Left, Right, Suck, NoOp};
-unsigned int time = 0;
+unsigned int rounds = 0;
 bool** world;
 
+struct coord{
+  unsigned int x;
+  unsigned int y;
+};
+
+void welcome();
+coord define_world();
+void initialize_world(coord);
+void free_world(coord);
+void print_world(coord);
+
 int main() {
+  welcome();
+
+  coord size = define_world();
+
+  initialize_world(size);
+
+  print_world(size);
+  
+  free_world(size);
+  
+  return 0;
+}
+
+void welcome() {
   // Program title
   std::cout << "*** Intelligent Vacuum Cleaner Simulator ***" << std::endl;
+}
 
+coord define_world() {
+  coord world;
+  
   // Fetch world
   std::cout << " - World Definition -" << std::endl;
   std::cout << "   Rows: ";
-  unsigned int rows;
-  std::cin >> rows;
+  std::cin >> world.x;
   std::cin.get();
+  
   std::cout << "   Columns: ";
-  unsigned int columns;
-  std::cin >> columns;
+  std::cin >> world.y;
   std::cin.get();
-  std::cout << "World: " << rows << "x" << columns << "." << std::endl;
-  std::cout << "Done." << std::endl;
+  std::cout << "World: " << world.x << "x" << world.y << "." << std::endl;
 
+  return world;
+}
+
+void initialize_world(coord size) {
   // Initialize world array
-  world = new bool*[rows];
-  for(int i = 0; i < rows; i++)
-    world[i] = new bool[columns];
+  world = new bool*[size.x];
+  for(int i = 0; i < size.x; i++)
+    world[i] = new bool[size.y];
 
   // Seed random number generator
   srand(time(NULL));
 
   // Populate world array
-  for(int x = 0; x < rows; x++)
-    for(int y = 0; y < columns; y++)
+  for(int x = 0; x < size.x; x++)
+    for(int y = 0; y < size.y; y++)
       world[x][y] = rand() % 2;
+}
 
+void free_world(coord size) {
+  // Free world array
+  for(int i = 0; i < size.x; i++)
+    delete [] world[i];
+  delete [] world;
+}
+
+void print_world(coord size) {
   // Print world array
-  for(int x = 0; x < rows; x++) {
-    for(int y = 0; y < columns; y++)
+  for(int x = 0; x < size.x; x++) {
+    for(int y = 0; y < size.y; y++)
       std::cout << world[x][y];
     std::cout << std::endl;
   }
-
-  // Free world array
-  for(int i = 0; i < rows; i++)
-    delete [] world[i];
-  delete [] world;
-  
-  return 0;
 }
